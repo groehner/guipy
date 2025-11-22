f85862b71b915567f557b2ce5d53345e4aee6939
Gerhard Röhner <groehner@t-online.de>
2025-11-22 19:38:33 +0100

    Version 8.01

--- Source/Main/UUpdate.pas
+++ Source/Main/UUpdate.pas
@@ -11,23 +11,23 @@ uses
   dlgPyIDEBase;
 
 const
-  LastHandeldPyScripterCommit = 'Oct 27, 2025';
+  LastHandeldPyScripterCommit = 'Nov 20, 2025';
   Server  = 'https://guipy.de/download/';
   InfFile = Server + 'version.txt';
   {$IFDEF WIN32}
   Zipfile = 'GuiPy.zip';   // update portable version
   Setupfile = 'GuiPy-%s-x86-Setup.exe';   // update default version
-  Version = '8.00, 32 Bit';
+  Version = '8.01, 32 Bit';
   Bits = '32';
   {$ENDIF}
   {$IFDEF WIN64}
   Zipfile = 'GuiPy64.zip';
   Setupfile = 'GuiPy-%s-x64-Setup.exe';
-  Version = '8.00, 64 Bit';
+  Version = '8.01, 64 Bit';
   Bits = '64';
   {$ENDIF}
 
-  Day   = 3;
+  Day   = 22;
   Month = 11;
   Year  = 2025;
 
